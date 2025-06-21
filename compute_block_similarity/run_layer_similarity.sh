a138ef8d96ec2e3fc169e21001a3a6fa4120a384
chang <changlee99@gmail.com>
2025-06-21 11:13:33 +0900

    aa

--- compute_block_similarity/run_layer_similarity.sh
+++ compute_block_similarity/run_layer_similarity.sh
@@ -3,11 +3,11 @@
 # This Bash script runs the Python script with arguments
 
 # Run the Python script with command-line arguments
-python layer_similarity.py --model_path "mistralai/Mistral-7B-Instruct-v0.2" \
+python layer_similarity.py --model_path "/home/chang/t9/StockModels/google-gemma-3-12b-it" \
                       --dataset "arcee-ai/sec-data-mini" \
                       --dataset_column "text" \
                       --batch_size 8 \
                       --max_length 1024 \
-                      --layers_to_skip 8 \
+                      --layers_to_skip 6 \
                       --dataset_size 4000 \
                       --dataset_subset "train" 
