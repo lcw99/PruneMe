60fef961bd6aa92a91dd676f6b5440cae6cd0369
lcw99 <changlee99@gmail.com>
2025-06-21 13:35:14 +0900

    aa

--- compute_block_similarity/run_layer_similarity.sh
+++ compute_block_similarity/run_layer_similarity.sh
@@ -3,11 +3,20 @@
 # This Bash script runs the Python script with arguments
 
 # Run the Python script with command-line arguments
-python layer_similarity.py --model_path "/home/chang/t9/StockModels/google-gemma-3-27b-it" \
+python layer_similarity.py --model_path "/home/chang/t9/StockModels/google-gemma-3-12b-it" \
                       --dataset "arcee-ai/sec-data-mini" \
                       --dataset_column "text" \
                       --batch_size 8 \
                       --max_length 1024 \
-                      --layers_to_skip 6 \
+                      --layers_to_skip 24 \
                       --dataset_size 4000 \
                       --dataset_subset "train" 
+
+# python layer_similarity.py --model_path "/home/chang/t9/StockModels/google-gemma-3-27b-it" \
+#                       --dataset "arcee-ai/sec-data-mini" \
+#                       --dataset_column "text" \
+#                       --batch_size 8 \
+#                       --max_length 1024 \
+#                       --layers_to_skip 30 \
+#                       --dataset_size 4000 \
+#                       --dataset_subset "train"                       
